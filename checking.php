23630c0f3761f0243c89eb8268a1cb7ef6e0bbcb
lenapochemy <redmirise@gmail.com>
2023-09-05 15:29:54 +0300

    Initial commit

---
+++ checking.php
@@ -0,0 +1,45 @@
+<?php
+    $start_time = microtime();
+    $x = $_GET['x'];
+    $y = $_GET['y'];
+    $r = $_GET['R'];
+    $flag = true;
+    $result = "";
+    $response = "";
+
+
+    if($_SERVER['REQUEST_METHOD'] === 'GET') {
+        if (!preg_match('/^-?\d+(\.|,)?\d*$/', $x) ||
+            !preg_match('/^-?\d+(\.|,)?\d*$/', $y) ||
+            !preg_match('/^-?\d+(\.|,)?\d*$/', $r))
+            $flag = false;
+
+
+        if ($y < -3 || $y > 3)
+            $flag = false;
+
+
+        if ((($x * $x + $y * $y) <= $r * $r && $x >= 0 && $y <= 0) ||
+            (0 <= $x && $x <= $r && 0 <= $y && $y <= $r / 2) ||
+            ($y >= (-$x - $r / 2) && $x <= 0 && $y <= 0)) {
+            $result = "Ура! Точка попала в область";
+        } else {
+            $result = "Ой... Точка не попала в область";
+        }
+
+        $response .= $flag;
+        $response .= ";";
+        $response .= $x;
+        $response .= ";";
+        $response .= $y;
+        $response .= ";";
+        $response .= $r;
+        $response .= ";";
+        $response .= $result;
+        $response .= ";";
+        $response .= date("Y-m-d H:i:s");
+        $response .= ";";
+        $response .= microtime() - $start_time;
+        $response .= "/";
+        echo $response;
+    }
